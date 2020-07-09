9afa7cf247bf58c8bb176fae18078c8ebc8bde44
TheMshary <themshary94@gmail.com>
2020-07-09 16:37:32 +0300

    Ducky now clickable

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./rubber-ducky.png";
 import "./App.css";
 
 function App() {
+  const [duckies, setDuckies] = useState(0);
+  const addDucky = () => setDuckies(duckies + 1);
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          <code>0</code> Duckies!
+          <code>{duckies}</code> Duckies!
         </p>
-        <img src={logo} className="App-logo" alt="logo" />
+        <div onClick={addDucky}>
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
         <p>Click the Ducky!</p>
       </header>
     </div>
