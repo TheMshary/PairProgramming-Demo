307e93470609678b09d85a3fb1aa35eb53f7c828
TheMshary <themshary94@gmail.com>
2020-07-09 15:34:07 +0300

    Revert code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import logo from "./rubber-ducky.png";
 import "./App.css";
 
 function App() {
-  const [duckies, setDuckies] = useState(0);
-  const addDucky = () => setDuckies(duckies + 1);
   return (
     <div className="App">
       <header className="App-header">
-        <code>{duckies}</code>
-        <div onClick={addDucky}>
-          <img src={logo} className="App-logo" alt="logo" />
-        </div>
+        <p>
+          <code>0</code> Duckies!
+        </p>
+        <img src={logo} className="App-logo" alt="logo" />
         <p>Click the Ducky!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
